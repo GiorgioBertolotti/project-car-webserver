a53d7d70ed8254d1f05e6b1ff152e4ab43d468bc
Giorgio <giorgiobertol@gmail.com>
2017-02-12 11:44:49 +0100

    Sistemata funzione di logout per eliminare anche il token legato alla sessione

--- db_communication.php
+++ db_communication.php
@@ -160,6 +160,10 @@ function logoutUser($data){
 		$queryt = "UPDATE user SET Type_id = NULL WHERE id = '".$id."'";
 		if(!mysql_query($queryt,$conn))
 			API_Response(true,"Errore nella query",__FUNCTION__);
+		// Delete token from User_Token
+		$queryto = "DELETE FROM user_token WHERE Token='".$logout_data->token."'";
+		if(!mysql_query($queryto,$conn))
+			API_Response(true,"Errore nella query",__FUNCTION__);
 		API_Response(false,"Logout eseguito con successo",__FUNCTION__);
 	}
 	else
