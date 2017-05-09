bc17f2b2f85c60fa3b33ad85050c0ae865254729
Giorgio <giorgiobertol@gmail.com>
2017-05-09 22:52:27 +0200

    Aggiunto WebSocketServer che ascolta attraverso rete sicura

---
+++ EasyTravelWSServer/EasyTravelWSServer/Client.cs
@@ -0,0 +1,17 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTravelWSServer
+{
+    class Client
+    {
+        public string Mobile { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public IWebSocketConnection Socket { get; set; }
+    }
+}
